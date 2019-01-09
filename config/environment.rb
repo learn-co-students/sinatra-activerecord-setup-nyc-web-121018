467265f6c38357dc0acd0a8e0f8c56484f59f21e
Oli Nelson <olivernelson@icloud.com>
2019-01-09 09:43:00 -0500

    first commit

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,12 @@
 ENV['SINATRA_ENV'] ||= "development"
 
 require 'bundler/setup'
+require './config/environment'
+require 'sinatra/activerecord/rake'
 Bundler.require(:default, ENV['SINATRA_ENV'])
+
+configure :development do
+  set :database, 'sqlite3:db/database.db'
+end
 
 require './app'
